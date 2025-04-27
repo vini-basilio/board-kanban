3359187d9f153c6926619f8f983fa2a08acea228
Vini <vini.ba.dev@gmail.com>
2025-04-27 20:32:18 -0300

    Refactor VCS configuration and update Lombok version for compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,15 +9,20 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 dependencies {
     implementation("org.liquibase:liquibase-core:4.29.1")
     implementation("mysql:mysql-connector-java:8.0.33")
 
-
-    implementation("org.projectlombok:lombok:1.18.28")
-    annotationProcessor("org.projectlombok:lombok:1.18.28")
-    testImplementation("org.projectlombok:lombok:1.18.28")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.28")
+    // Updated Lombok version for Java 23 compatibility
+    implementation("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    testImplementation("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
 }
 
 tasks.test {
