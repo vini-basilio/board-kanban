ad9a838aed2b5ce4d98afddbf3476d16dc96527e
Vini <vini.ba.dev@gmail.com>
2025-04-27 13:39:07 -0300

    Implement findByBoardId method in BoardColumnDAO and enhance BoardColumnKindEnum with findByName method

--- src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
+++ src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
@@ -2,11 +2,13 @@ package br.com.dio.persistence.dao;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.mysql.cj.jdbc.StatementImpl;
 
 import br.com.dio.persistence.entity.BoardColumnEntity;
+import br.com.dio.persistence.entity.BoardColumnKindEnum;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -32,6 +34,27 @@ public class BoardColumnDAO {
     }
 
     public List<BoardColumnEntity> findByBoardId(final Long id) throws SQLException {
-        return null;
+        var sql = "SELECT id, name, `order` FROM BOARDS_COLUMN WHERE board_id = ? ORDER BY `order`";
+
+        List<BoardColumnEntity> entities = new ArrayList<>();
+
+        try (var statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, id);
+            statement.executeQuery();
+
+            var resultSet = statement.getResultSet();
+
+            while (resultSet.next()) {
+                var entity = new BoardColumnEntity();
+                entity.setId(resultSet.getLong("id"));
+                entity.setName(resultSet.getString("name"));
+                entity.setOrder(resultSet.getInt("order"));
+                entity.setKind(BoardColumnKindEnum.findByName(resultSet.getString("kind")));
+                entities.add(entity);
+            }
+
+            return entities;
+        }
+
     }
 }
--- src/main/java/br/com/dio/persistence/entity/BoardColumnKindEnum.java
+++ src/main/java/br/com/dio/persistence/entity/BoardColumnKindEnum.java
@@ -1,8 +1,19 @@
 package br.com.dio.persistence.entity;
 
+import java.util.stream.Stream;
+
 public enum BoardColumnKindEnum {
     INITIAL,
     FINAL,
     CANCEL,
-    PEDING
+    PEDING;
+
+    public static BoardColumnKindEnum findByName(final String name) {
+
+        return Stream.of(BoardColumnKindEnum.values())
+                .filter(e -> e.name().equals(name))
+                .findFirst()
+                .orElseThrow();
+
+    }
 }
