2bfde00048de8c743df6abc0c45c145ab1de12b7
Vini <vini.ba.dev@gmail.com>
2025-04-27 11:47:46 -0300

    Refactor BlockEntity fields for consistency and clarity

--- src/main/java/br/com/dio/persistence/entity/BlockEntity.java
+++ src/main/java/br/com/dio/persistence/entity/BlockEntity.java
@@ -2,8 +2,13 @@ package br.com.dio.persistence.entity;
 
 import java.time.OffsetDateTime;
 
+import lombok.Data;
+
 @Data
 public class BlockEntity {
     private Long id;
-    private OffsetDateTime blocked_at;
+    private OffsetDateTime blockedAt;
+    private String blockReason;
+    private OffsetDateTime unblockedAt;
+    private String unblockedReason;
 }
